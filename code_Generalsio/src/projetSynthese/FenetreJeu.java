5c33d8789ac209d6630097b6e13ced734577900b
Unknown <willemin.florent@gmail.com>
2017-05-14 14:48:10 +0200

    Génération du décor aléatoirement

--- code_Generalsio/src/projetSynthese/FenetreJeu.java
+++ code_Generalsio/src/projetSynthese/FenetreJeu.java
@@ -1,5 +1,8 @@
 package projetSynthese;
 
+import java.util.ArrayList;
+import java.io.*; 
+import java.lang.Math;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
 import javax.swing.*;
@@ -8,24 +11,31 @@ import java.awt.event.*;
  
 public class FenetreJeu extends JFrame
 {
-	private int LIGNE=18,COLONNE=18;
+	private int COLONNE=18,LIGNE=18;
 	
 	private JLabel[][] JL_cases; // tableau de JLabels
 	private JPanel JP_Grille = new JPanel(); 	// panel du bas ( grille )
-	private GridLayout GL_Matrice = new GridLayout(LIGNE,COLONNE); 	
+	private GridLayout GL_Matrice = new GridLayout(COLONNE,LIGNE); 	
 	
 	private String dossierIcone = "Icone/";
 	
+	private int nbrAlea;
+	private int xAlea,yAlea;
 	private String tour="tour.png";			
 	private String chateau="chateau.png";
 	private String montagne="montagne.png";
 	
+	private int NBRMONTAGNE=65;
+	private int NBRTOUR=22;
+	
+	private boolean tabCoord[][];
+
 	Font font = new Font("Calibri",Font.CENTER_BASELINE,21);	//police écrite sur les cases
 	
 	//initialise la surface de jeu
 	public FenetreJeu()
 	{
-		JL_cases = new JLabel[LIGNE][COLONNE];	 // création du tableau de JLabel
+		JL_cases = new JLabel[COLONNE][LIGNE];	 // création du tableau de JLabel
 		this.getContentPane().setLayout(null);   // créé bandeau vide
 		this.setSize(new Dimension(1000, 800)); //Taille fenetre entiere
 		this.setTitle("Generalsio");			 //Titre
@@ -35,9 +45,9 @@ public class FenetreJeu extends JFrame
 		JP_Grille.setLayout(GL_Matrice);
 		this.add(JP_Grille);	
 
-		for (int ligne = 0; ligne < LIGNE; ligne++)
+		for (int ligne = 0; ligne < COLONNE; ligne++)
 		{
-			for (int colonne = 0; colonne < COLONNE; colonne++)
+			for (int colonne = 0; colonne < LIGNE; colonne++)
 			{
 				JL_cases[colonne][ligne] = new JLabel(); // creation du JLabel
 				JP_Grille.add(JL_cases[colonne][ligne]); // ajouter au Panel
@@ -46,105 +56,66 @@ public class FenetreJeu extends JFrame
 			}	
 		}
 		InitCarte();
+		
 	}
 	
 	private void InitCarte()
 	{
-		setIcon(chateau, JL_cases[1][3]);
-		setIcon(chateau, JL_cases[13][15]);
-		setCouleur(Color.BLUE, JL_cases[1][3]);
-		setCouleur(Color.RED, JL_cases[13][15]);
-		setTexte("1",JL_cases[1][3]);
-		setTexte("1",JL_cases[13][15]);
 		
-		 setImgTour(JL_cases[0][1]);
-		 setImgTour(JL_cases[0][15]);
-		 setImgTour(JL_cases[3][9]);
-		 setImgTour(JL_cases[0][11]);
-		 setImgTour(JL_cases[10][0]);
-		 setImgTour(JL_cases[3][3]);
-		 setImgTour(JL_cases[6][2]);
-		 setImgTour(JL_cases[3][1]);
-		 setImgTour(JL_cases[1][17]);
-		 setImgTour(JL_cases[2][14]);
-		 setImgTour(JL_cases[4][5]);
-		 setImgTour(JL_cases[5][13]);
-		 setImgTour(JL_cases[7][14]);
-		 setImgTour(JL_cases[8][11]);
-		 setImgTour(JL_cases[10][17]);
-		 setImgTour(JL_cases[11][5]);
-		 setImgTour(JL_cases[12][8]);
-		 setImgTour(JL_cases[13][10]);
-		 setImgTour(JL_cases[16][3]);
-		 setImgTour(JL_cases[17][17]);
-		 setImgTour(JL_cases[15][14]);
-	
-		setImgMontagne(JL_cases[0][0]);
-		setImgMontagne(JL_cases[0][4]);
-		setImgMontagne(JL_cases[0][6]);
-		setImgMontagne(JL_cases[0][7]);
-		setImgMontagne(JL_cases[0][8]);
-		setImgMontagne(JL_cases[0][12]);
-		setImgMontagne(JL_cases[1][5]);
-		setImgMontagne(JL_cases[1][11]);
-		setImgMontagne(JL_cases[1][14]);
-		setImgMontagne(JL_cases[1][15]);
-		setImgMontagne(JL_cases[2][1]);
-		setImgMontagne(JL_cases[2][4]);
-		setImgMontagne(JL_cases[2][7]);
-		setImgMontagne(JL_cases[2][9]);
-		setImgMontagne(JL_cases[2][17]);
-		setImgMontagne(JL_cases[3][7]);
-		setImgMontagne(JL_cases[3][10]);
-		setImgMontagne(JL_cases[3][12]);
-		setImgMontagne(JL_cases[3][14]);
-		setImgMontagne(JL_cases[3][15]);
-		setImgMontagne(JL_cases[4][8]);
-		setImgMontagne(JL_cases[4][12]);
-		setImgMontagne(JL_cases[4][13]);
-		setImgMontagne(JL_cases[5][1]);
-		setImgMontagne(JL_cases[5][12]);
-		setImgMontagne(JL_cases[6][7]);
-		setImgMontagne(JL_cases[6][8]);
-		setImgMontagne(JL_cases[6][15]);
-		setImgMontagne(JL_cases[7][2]);
-		setImgMontagne(JL_cases[7][3]);
-		setImgMontagne(JL_cases[8][1]);
-		setImgMontagne(JL_cases[8][2]);
-		setImgMontagne(JL_cases[8][3]);
-		setImgMontagne(JL_cases[8][4]);
-		setImgMontagne(JL_cases[8][5]);
-		setImgMontagne(JL_cases[8][6]);
-		setImgMontagne(JL_cases[8][14]);
-		setImgMontagne(JL_cases[8][15]);
-		setImgMontagne(JL_cases[8][16]);
-		setImgMontagne(JL_cases[9][0]);
-		setImgMontagne(JL_cases[9][9]);
-		setImgMontagne(JL_cases[9][10]);
-		setImgMontagne(JL_cases[9][14]);
-		setImgMontagne(JL_cases[11][1]);
-		setImgMontagne(JL_cases[12][3]);
-		setImgMontagne(JL_cases[12][4]);
-		setImgMontagne(JL_cases[12][5]);
-		setImgMontagne(JL_cases[12][12]);
-		setImgMontagne(JL_cases[12][13]);
-		setImgMontagne(JL_cases[12][14]);
-		setImgMontagne(JL_cases[13][0]);
-		setImgMontagne(JL_cases[13][6]);
-		setImgMontagne(JL_cases[13][7]);
-		setImgMontagne(JL_cases[14][6]);
-		setImgMontagne(JL_cases[15][3]);
-		setImgMontagne(JL_cases[15][12]);
-		setImgMontagne(JL_cases[16][12]);
-		setImgMontagne(JL_cases[17][0]);
-		setImgMontagne(JL_cases[17][5]);
-		setImgMontagne(JL_cases[17][6]);
-		setImgMontagne(JL_cases[17][7]);
-		setImgMontagne(JL_cases[17][12]);
-		setImgMontagne(JL_cases[14][17]);
-		setImgMontagne(JL_cases[15][17]);
+		tabCoord = new boolean[18][18];	//tableau pour tester les cases occupées
+		
+		for (int i=0; i<18; i++)
+		{
+			for (int j=0; j<18; j++)
+			{
+				tabCoord[i][j]=false;
+			}
+		}
+		
+		xAlea=(int) (Math.random()*(COLONNE));  //Math.random()*( max - mini + 1 ) ) + mini;
+		yAlea=(int) (Math.random()*((LIGNE-2)/2));  //Math.random()*( max - mini + 1 ) ) + mini;
+		tabCoord[xAlea][yAlea]=true;
+		setIcon(chateau, JL_cases[xAlea][yAlea]);  // on pop la première base dans la moitié supérieur de la carte
+		setCouleur(Color.BLUE, JL_cases[xAlea][yAlea]);
+		setTexte("1",JL_cases[xAlea][yAlea]);
+		
+		xAlea=(int) (Math.random()*(COLONNE));  //Math.random()*( max - mini + 1 ) ) + mini;		
+		yAlea=(int) (Math.random()*((LIGNE-1)-((LIGNE+2)/2))+((LIGNE+2)/2));  //Math.random()*( max - mini + 1 ) ) + mini;
+		tabCoord[xAlea][yAlea]=true;
+		setIcon(chateau, JL_cases[xAlea][yAlea]);  // on pop la deuxième base dans la moitié inférieur de la carte
+		setCouleur(Color.RED, JL_cases[xAlea][yAlea]);
+		setTexte("1",JL_cases[xAlea][yAlea]);
+
+		for (int i=0; i< NBRTOUR; i++)
+		{
+			xAlea=(int) (Math.random()*(COLONNE));  //Math.random()*( max - mini + 1 ) ) + mini;
+			yAlea=(int) (Math.random()*(LIGNE));  //Math.random()*( max - mini + 1 ) ) + mini;}
+			if (tabCoord[xAlea][yAlea] == false)
+			{
+				setImgTour(JL_cases[xAlea][yAlea]);
+				tabCoord[xAlea][yAlea]=true;		
+			}
+			else
+			{
+				i--;
+			}
+		}
+		
+		for (int i=0; i< NBRMONTAGNE; i++)
+		{
+			xAlea=(int) (Math.random()*(COLONNE));  //Math.random()*( max - mini + 1 ) ) + mini;
+			yAlea=(int) (Math.random()*(LIGNE));  //Math.random()*( max - mini + 1 ) ) + mini;
+			if (tabCoord[xAlea][yAlea] == false)
+			{
+				setImgMontagne(JL_cases[xAlea][yAlea]);
+				tabCoord[xAlea][yAlea]=true;
+			}
+			else
+			{
+				i--;
+			}
+		}		
 	}
-	
 	
 	private void setImgMontagne(JLabel position)
 	{
@@ -158,7 +129,8 @@ public class FenetreJeu extends JFrame
 		Color color1= new Color(180,180,180);
 		setIcon(tour,position);
 		setCouleur(color1,position);
-		setTexte("40",position);
+		nbrAlea=(int) (Math.random()*(16)+40);
+		setTexte(Integer.toString(nbrAlea),position);
 	}
 	
 	private void setIcon(String element, JLabel position)
