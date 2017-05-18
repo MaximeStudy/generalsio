f701fc66dfc9a12d7b023386f494b46227fd551f
MaximeStudy <maximestudy@gmail.com>
2017-05-18 17:30:25 +0200

    removed folder UML and create a reverse engineering project with Eclipse

---
+++ Projet_GeneralsIo/src/model/Base.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Base extends Chateau {
+    /**
+     * @attribute
+     */
+    public Base(int x,int y) {
+        super(x,y);
+    }
+}
---
+++ Projet_GeneralsIo/src/model/Chateau.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Chateau extends Plaine {
+    /**
+     * @attribute
+     */
+    
+    public Chateau(int x,int y) {
+        super(x,y);
+    }
+
+}
